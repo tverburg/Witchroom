5f4e97187402bc166e793c21b2673acfc1a49892
Tetherys <woaortom@gmail.com>
2023-12-17 14:50:45 +0100

    Update 3_MqttEthernet.ino

--- Sketches/CabinDoor/3_MqttEthernet.ino
+++ Sketches/CabinDoor/3_MqttEthernet.ino
@@ -2,7 +2,9 @@
 char msg[64];
 // The topic to which to subscribe for actions or updates
 char listenerTopic[32];
-char publishingTopic[32];
+long lastReconnectAttempt = 0;
+
+PubSubClient client(server, 1883, callback, ethClient);
 
 void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print(F("Message arrived ["));
@@ -12,9 +14,56 @@ void callback(char* topic, byte* payload, unsigned int length) {
     Serial.print((char)payload[i]);
   }
   Serial.println();
+
+  if (strcmp(topic,subTopics[0])==0){
+    //pingrequest
+    client.publish(pubTopics[0], "connected");
+  }
+  else if (strcmp(topic,subTopics[1])==0){
+    //solve door
+    onSolve();
+  } 
+  else if (strcmp(topic,subTopics[2])==0){
+    // reset (lock) door
+    onReset();
+  } 
+  else if (strcmp(topic,subTopics[3])==0){
+    // solve (open) cage
+    openCage(1);
+  } 
+  else if (strcmp(topic,subTopics[4])==0){
+    // reset (close) cage
+    closeCage();
+  } 
 }
 
-PubSubClient client(server, 1883, callback, ethClient);
+boolean reconnect() {
+  Serial.println("attempt reconnect");
+    if (client.connect(deviceID, "shape", "escape")) {
+      Serial.println(F("connected. start subscriptions"));
+      client.publish(pubTopics[0], "connected");
+      // Subscribe to topics meant for this device
+      
+      for (int i = 0 ; i < numberOfSubscriptions; i++) {
+        Serial.print("Subscription for '");
+        Serial.print(subTopics[i]);
+
+        bool subscriptionStatus= client.subscribe(subTopics[i], 1);
+
+        if(!subscriptionStatus) {
+          Serial.print("' failed!");
+        }
+        Serial.println(".");
+      }
+
+      // Subscribe to topics meant for all devices
+      client.subscribe("witchroom/all");
+    } else {
+      Serial.println(F("client not connected: "));
+      Serial.println(client.state());
+    };
+  return client.connected();
+}
 
 bool valueToBool(int value) {
   if(value == 1) {
@@ -24,64 +73,31 @@ bool valueToBool(int value) {
   }
 }
 
-void onSolve() {
-  //#ifdef DEBUG
-    //Serial.println("Puzzle has just been solved");
-  //#endif
-
-  // todo: do something with output pin
-
-  // Update the global puzzle state
-  puzzleState = Solved;
-
-  // publish message to MQTT broker
-  client.publish(publishingTopic, "solved");
-}
-
-void onUnSolve() {
-  //#ifdef DEBUG
-    //Serial.println("Puzzle has just become unsolved");
-  //#endif
-
-   // todo: do something with output pin
-
-  // Update the global puzzle state
-  puzzleState = Running;
-
-  // publish message to MQTT broker
-  client.publish(publishingTopic, "unsolved");
-}
-
-void onReset() {
-  //#ifdef DEBUG
-    //Serial.println(F("Puzzle has just been reset!"));
-  //#endif
-
-  // todo: reset the output pin
-
-  // Update the global puzzle state
-  puzzleState = Running;
-
-  // publish message to MQTT broker
-  client.publish(publishingTopic, "reset");
-}
-
 void mqttEthernetSetup() {
-  //set the topics for this device
-  snprintf(listenerTopic, 32, "%s/%s/control", nameSpace, deviceID);
-  snprintf(publishingTopic, 32, "%s/%s/update", nameSpace, deviceID);
+  Serial.println(F("mqttEthernetSetup"));
 
   Ethernet.begin(mac, ip);
-  // Note - the default maximum packet size is 128 bytes. If the
-  // combined length of clientId, username and password exceed this use the
-  // following to increase the buffer size:
-  // client.setBufferSize(255);
-  
-  if (client.connect(deviceID, "shape", "escape")) {
-    client.publish(publishingTopic, "connected");
-    // Subscribe to topics meant for this device
-    client.subscribe(listenerTopic);
-    // Subscribe to topics meant for all devices
-    client.subscribe("witchroom/all");
-  };
+  delay(1500);
+
+  lastReconnectAttempt = 0;
+  Serial.println(F("mqttEthernetSetup done"));
 };
+
+void mqttLoop()
+{
+  if (!client.connected()) {
+    long now = millis();
+    if (now - lastReconnectAttempt > 5000) {
+      lastReconnectAttempt = now;
+      // Attempt to reconnect
+      if (reconnect()) {
+        lastReconnectAttempt = 0;
+      }
+    }
+  } else {
+    // Client connected
+    client.loop();
+     //Ethernet.maintain();
+  }
+ 
+}
